8afa8fe729d52fe88d36b639dfa9606223ea1fcd
Arjun <arjun.bali96@gmail.com>
2014-06-03 17:32:59 -0400

    length of list, wrote banana statements, and puts the second item of the list

--- exercise6.rb
+++ exercise6.rb
@@ -9,3 +9,12 @@ list(grocery_list)
 grocery_list << "rice"
 list(grocery_list)
 
+puts "The length of your list is #{grocery_list.length}"
+
+if grocery_list.include?("bananas")
+	puts "You need to pick up bananas."
+else
+	puts "You don't need to pick up bananas today."
+end
+
+puts grocery_list[1]
