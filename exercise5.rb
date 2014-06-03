d0deb4214e8e57e8eeef71f0065975a3e63d9724
Arjun <arjun.bali96@gmail.com>
2014-06-03 16:37:00 -0400

    defined variable

--- exercise5.rb
+++ exercise5.rb
@@ -1,3 +1,7 @@
 puts "Enter the temperature in Farenheit."
 farenheit = gets.chomp.to_i
 
+def convert(farenheit)
+	return (farenheit-32)*(5.0/9) # use 5.0 because you need the result to be a float!)
+end
+
