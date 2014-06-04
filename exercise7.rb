dfe5dc472cbbf5c58df94cbbe8b86440f41a9744
Arjun <arjun.bali96@gmail.com>
2014-06-03 22:39:16 -0400

    finished BONUS, putting the total number of students in all cohorts, AND fixed question 2

--- exercise7.rb
+++ exercise7.rb
@@ -1,7 +1,7 @@
 students = {
 	:cohort1 => 34,
 	:cohort2 => 42,
-	:cohort4 => 22
+	:cohort3 => 22
 }
 
 #Question 1
@@ -14,17 +14,26 @@ end
 list(students)
 
 #Question 2
-students[:cohort4] += 43
-puts students[:cohort4]
+students[:cohort4] = 43
 
 #Question 3
 puts students.keys
 
 #Question 4
-students.each_value {|number|
+students.each_value do |number|
 	puts (number*1.05).to_i
-}
+end
 
 #Question 5
 students.delete(:cohort2)
 list(students)
+
+#Question 6
+students[:cohort2] = 42
+number = students.values
+sum = 0 #assign the variable sum to the value 0
+number.each do |x|
+	sum += x
+end
+
+puts sum
